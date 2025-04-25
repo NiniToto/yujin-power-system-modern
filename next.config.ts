c95cb1582729a6d70f57d0a9828454fb4551e3eb
NiniToto <ljm911209@gmail.com>
2025-04-25 21:36:14 +0900

    린트 검사 제외외

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: ".next"
+  distDir: ".next",
+  eslint: {
+    // 빌드 시 ESLint 검사를 무시하도록 설정
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
